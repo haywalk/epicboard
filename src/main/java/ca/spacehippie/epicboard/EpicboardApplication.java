e03054a4ed409a749d6caf213900c5a79cb74f81
Hayden Walker <haydenwalker@live.com>
2025-07-23 17:22:26 +0300

    Barebones REST API

--- src/main/java/ca/spacehippie/epicboard/EpicboardApplication.java
+++ src/main/java/ca/spacehippie/epicboard/EpicboardApplication.java
@@ -2,12 +2,32 @@ package ca.spacehippie.epicboard;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * REST API for EpicBoard.
+ * 
+ * @version 2025-07-23
+ */
 @SpringBootApplication
+@RestController
+@RequestMapping("api") // prefix /api/ for all endpoints
 public class EpicboardApplication {
-
+	/**
+	 * Start the program.
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(EpicboardApplication.class, args);
 	}
 
+	/**
+	 * Bare-bones endpoint /api/helloworld 
+	 * @return  "Hello, world!"
+	 */
+	@GetMapping("helloworld")
+	public String helloWorld() {
+		return "Hello, world!";
+	}
 }
