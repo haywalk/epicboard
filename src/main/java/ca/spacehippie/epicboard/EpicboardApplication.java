445a572e82294f97e36a847d9a44652a42abac93
Hayden Walker <haydenwalker@live.com>
2025-07-23 16:38:17 +0300

    Initialize Gradle web project

---
+++ src/main/java/ca/spacehippie/epicboard/EpicboardApplication.java
@@ -0,0 +1,13 @@
+package ca.spacehippie.epicboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EpicboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EpicboardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ca/spacehippie/epicboard/EpicboardApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.spacehippie.epicboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EpicboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
