f70b693ce546ad9852457f11152860ee0aba39fd
Hayden Walker <haydenwalker@live.com>
2025-07-23 14:56:09 -0300

    Pseudocode for each endpoint

--- src/main/java/ca/spacehippie/epicboard/EpicboardApplication.java
+++ src/main/java/ca/spacehippie/epicboard/EpicboardApplication.java
@@ -43,6 +43,8 @@ public class EpicboardApplication {
 	@GetMapping("board")
 	public String getBoard(@RequestParam(value = "id", defaultValue="") String id) {
 		return "json index of board " + id;
+		// SELECT * FROM posts WHERE BOARD = id
+		// return as JSON 
 	}
 
 	/**
@@ -54,6 +56,8 @@ public class EpicboardApplication {
 	@GetMapping("thread")
 	public String getThread(@RequestParam(value = "id", defaultValue="") String id) {
 		return "json content of thread " + id;
+		// SELECT * FROM posts WHERE thread = id
+		// return as JSON
 	}
 
 	/**
@@ -65,6 +69,9 @@ public class EpicboardApplication {
 	@GetMapping("new")
 	public String newThread(@RequestParam(value="id", defaultValue="") String board) {
 		return "created new thread on board " + board;
+		// upload image (required for a thread)
+		// get board as JSON
+		// add post, update DB
 	}
 	
 	/**
@@ -76,5 +83,9 @@ public class EpicboardApplication {
 	@GetMapping("reply")
 	public String replyThread(@RequestParam(value="id", defaultValue="") String id) {
 		return "replied to thread " + id;
+		// upload image if applicable
+		// Get thread as JSON
+		// append reply
+		// update DB
 	}
 }
