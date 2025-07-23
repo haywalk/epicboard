2737072dc096a8bf4080b7dfb239bc88779c47b1
tbabineau <tbabine1@unb.ca>
2025-07-23 14:36:52 -0300

    Added DB config

---
+++ src/main/resources/DB/DB.sql
@@ -0,0 +1,11 @@
+DROP TABLE IF EXISTS posts;
+CREATE TABLE posts(
+    post_id INTO NOT NULL,
+    thread_id INT NOT NULL,
+    board varchar(15) NOT NULL,
+    post_time BIGINT(255) UNSIGNED NOT NULL,
+    content varchar(1023),
+    img_url varchar(127),
+    bump_index INT UNSIGNED NOT NULL,
+    PRIMARY KEY(post_id, thread_id)
+);
